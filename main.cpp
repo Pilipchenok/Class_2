c57f8d384447e0b1d69a8dc59f7cba10b85a72c7
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-12-05 13:16:19 +0300

    Added frame, etc

--- main.cpp
+++ main.cpp
@@ -18,11 +18,46 @@ namespace topit{
         p_t next(p_t) const override;
         p_t d;
     };
+
+    size_t points(const IDraw& d, p_t** pts, size_t& s);
+    f_t frame(const p_t* pts, size_t s);
 }
 int main(){
+    using topit::IDraw;
+    using topit::Dot;
     using topit::p_t;
-    p_t a{1, 0}, b{1, 0};
-    std::cout << (a != b) << "\n";
+    using topit::f_t;
+    int err = 0;
+    IDraw* shps[3] = {};
+    p_t* pts = nullptr;
+    size_t s = 0;
+    try{
+        shps[0] = new Dot(0, 0);
+        shps[1] = new Dot(5, 7);
+        shps[2] = new Dot(-5, -2);
+
+        for (size_t i = 0; i < 3; ++i){
+            s += points(*(shps[i]), &pts, s);
+        }
+        f_t fr;
+        // TODO:
+        // [1] достать все точки из фигур
+        // [2] посчитать ограничивающий прямоугольник
+        // [3]
+        // - 
+        // [4]
+        // - 
+        // [5]
+    }catch(...){
+        err = 2;
+        std::cerr << "Bad drawing" << "\n";
+    }
+
+    delete[] pts;
+    delete shps[0];
+    delete shps[1];
+    delete shps[2];
+    return err;
 }
 
 topit::Dot::Dot(p_t dd):
