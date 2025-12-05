afaa8dd5e51ed53ccbc82dab886119e2039427d8
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-12-05 12:39:25 +0300

    Added base structures

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main()
-{
+namespace topit{
+    struct p_t{ int x, y; };
+    bool operator==(p_t, p_t);
+    bool operator!=(p_t, p_t);
+    struct f_t { p_t aa, bb; };
+}
+int main(){
+    using topit::p_t;
+    p_t a{1, 0}, b{1, 0};
+    std::cout << (a != b) << "\n";
+}
 
+bool topit::operator==(p_t a, p_t b){
+    return a.x == b.x && a.y == b.y;
+}
+
+bool topit::operator!=(p_t a, p_t b){
+    return !(a == b);
 }
