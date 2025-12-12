f95bc9d145a3f228747288c872a23a3649fa19b7
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-12-12 17:56:04 +0300

    Added work from 12.12.2025

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ namespace topit{
     bool operator==(p_t, p_t);
     bool operator!=(p_t, p_t);
     struct f_t { p_t aa, bb; };
+    size_t rows(f_t fr);
+    size_t cols(f_t fr);
+
     struct IDraw{
         virtual ~IDraw() = default; 
         virtual p_t begin() const = 0;
@@ -19,6 +22,7 @@ namespace topit{
         p_t d;
     };
 
+    void extend(p_t** pts, size_t s, p_t p);
     size_t points(const IDraw& d, p_t** pts, size_t& s);
     f_t frame(const p_t* pts, size_t s);
     char* canvas(f_t fr, char fill);
@@ -41,6 +45,7 @@ namespace topit{
         p_t upper_left;
         p_t right_bottom;
     };
+
 }
 int main(){
     using topit::IDraw;
@@ -54,7 +59,7 @@ int main(){
     try{
         shps[0] = new Dot(0, 0);
         shps[1] = new Dot(5, 7);
-        shps[2] = new Dot(-5, -2);
+        shps[2] = new topit::VLine({2, 2}, {2, 10});
 
         for (size_t i = 0; i < 3; ++i){
             s += points(*(shps[i]), &pts, s);
@@ -76,6 +81,90 @@ int main(){
     delete shps[1];
     delete shps[2];
     return err;
+}
+
+size_t topit::rows(f_t fr)
+{
+    return fr.bb.y - fr.aa.y + 1;
+}
+
+size_t topit::cols(f_t fr){
+    return fr.bb.x - fr.aa.x + 1;
+}
+
+char* topit::canvas(f_t fr, char fill)
+{
+    char* cnv = new char[rows(fr) * cols(fr)];
+    for(size_t i = 0; i < rows(fr) * cols(fr); ++i)
+    {
+        cnv[i] = fill;
+    }
+    return cnv;
+}
+
+void topit::paint(char* cnv, f_t fr, p_t p, char fill)
+{
+    size_t dy = fr.bb.y - p.y;
+    size_t dx = p.x - fr.aa.x;
+    cnv[dy * cols(fr) + dx] = fill;
+}
+
+void topit::flush(std::ostream& os, const char* cnv, f_t fr)
+{
+    for(size_t i = 0; i < rows(fr); ++i)
+    {
+        for(size_t j = 0; j < cols(fr); ++j)
+        {
+            os << cnv[i * cols(fr) + j];
+        }
+        os << '\n';
+    }
+}
+
+topit::f_t topit::frame(const p_t* pts, size_t s)
+{
+    if(!s)
+    {
+        throw std::logic_error("no pts");
+    }
+    int minx = pts[0].x, maxx = minx;
+    int miny = pts[0].y, maxy = miny;
+    for(size_t i = 1; i < s; ++i)
+    {
+        minx = std::min(minx, pts[i].x);
+        maxx = std::max(maxx, pts[i].x);
+        miny = std::min(miny, pts[i].y);
+        maxy = std::max(maxy, pts[i].y);
+    }
+    p_t aa = {minx, miny};
+    p_t bb = {maxx, maxy};
+    return {aa, bb};
+}
+
+void topit::extend(p_t** pts, size_t s, p_t p)
+{
+    p_t* e = new p_t[s + 1];
+    for(size_t i = 0; i < s; ++i)
+    {
+        e[i] = (*pts)[i];
+    }
+    e[s] = p;
+    delete[] *pts;
+    *pts = e;
+}
+
+size_t topit::points(const IDraw& d, p_t** pts, size_t& s)
+{
+    size_t r = 1;
+    p_t p = d.begin();
+    extend(pts, s, p);
+    while(d.next(p) != d.begin())
+    {
+        p = d.next(p);
+        extend(pts, s + r, p);
+        ++r;
+    }
+    return r;
 }
 
 topit::Dot::Dot(p_t dd):
